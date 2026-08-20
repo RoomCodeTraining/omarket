ea389bcf5678e65d016594cf38954ca8ba06032e
Roomdada <dsieroger@gmail.com>
2026-08-20 16:09:57 +0000

    wip

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             AdminUserSeeder::class,
             SiteSettingsSeeder::class,
-            CatalogSeeder::class,
+            // CatalogSeeder::class,
         ]);
     }
 }
